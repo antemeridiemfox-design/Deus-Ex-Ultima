e0bc739f91ec06eb7abfd25a3ae85a211037a33a
antemeridiemfox-design <ante.meridiem.fox@gmail.com>
2026-02-02 00:39:23 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
 const PERMANENT_POSTS = [
  { title: "Sunset", text: "", audio: "Releases/The Sunset Is Beautiful.mp3", image: "Posters/Song Cover.JPG" },
+ { title: "Dark Demo", text: "", audio: "Releases/Dark Demo.mp3", image: "Posters/Dark_Demo.png" },
     // Paste generated snippets here. Example:
     // { title: "Example", text: "Description", audio: "tracks/song.mp3", image: "images/pic.jpg" },
 
 ];
+
 
